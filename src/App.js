66f5ed384417af71baf1b89c08ce32748cf98be9
JanaSharafaldeen <74385022+JanaSharafaldeen@users.noreply.github.com>
2020-11-13 11:50:55 +0200

    My first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import ToDo from "./component/ToDo";
+function App() {
+  return (
+      <div>
+        <ToDo />
+      </div>
+  );
+}
+
+export default App;
